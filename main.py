79ece2a8dbb09ab720a3948e2c5430808be21e16
renzoiwamoto-k <renzo.iwamoto@pucp.edu.pe>
2025-11-23 19:05:29 -0500

    langsmith setup and langchain basics

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 from dotenv import load_dotenv
-import os
-
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
 load_dotenv()
 
 
 def main():
     print("Hello from langchain-course!")
-    print(os.environ.get("OPEN_AI_API_KEY"))
 
+    prompt_template = """You are an expert of this topic: {topic}. 
+    Provide a detailed explanation about it.
+    """
+
+    template = PromptTemplate(
+        input_variables=["topic"],
+        template=prompt_template
+    )
+    
+    llm = ChatOpenAI(
+    temperature= 0.7,
+    model="gpt-3.5-turbo"
+    )
+
+    chain = template | llm
+    response = chain.invoke({"topic": "Artificial Intelligence"})
+    print(response.content)
 
 if __name__ == "__main__":
     main()
